10847239f66e35f2da30912d3e434404953552e7
Kalimar Maia <kmaia@customink.com>
2015-01-12 11:19:09 -0500

    Fix seeds.rb so the mmc fields are joined

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ MmcItem.destroy_all
 3.times do |w|
   mmc_vendor = MmcVendor.create(name: "mmc_vendor: #{w}")
   3.times do |x|
-    mmc_category = MmcCategory.create(name: "mmc_category: #{w}.#{x}")
+    mmc_category = MmcCategory.create(name: "mmc_category: #{w}.#{x}", mmc_vendor:  mmc_vendor)
     3.times do |y|
-      mmc_placement = MmcPlacement.create(name: "mmc_placement: #{w}.#{x}.#{y}")
+      mmc_placement = MmcPlacement.create(name: "mmc_placement: #{w}.#{x}.#{y}", mmc_category: mmc_category)
       3.times do |z|
-        mmc_item = MmcItem.create(name: "mmc_item: #{w}.#{x}.#{y}.#{z}")
+        mmc_item = MmcItem.create(name: "mmc_item: #{w}.#{x}.#{y}.#{z}", mmc_placement:mmc_placement)
       end
     end
   end
