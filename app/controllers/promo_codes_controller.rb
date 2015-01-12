611800a792740cb7389332d027e12fa8af9eecea
Kalimar Maia <kmaia@customink.com>
2015-01-12 14:09:59 -0500

    Debug promo_codes_controller

--- app/controllers/promo_codes_controller.rb
+++ app/controllers/promo_codes_controller.rb
@@ -1,10 +1,10 @@
 class PromoCodesController < ApplicationController
 
   def index
-    @mmc_vendors = MmcVendors.all
-    @mmc_categories = MmcCategories.all
-    @mmc_placements = MmcPlacements.all
-    @mmc_items = MmcItems.all
+    @mmc_vendors = MmcVendor.all
+    @mmc_categories = MmcCategory.all
+    @mmc_placements = MmcPlacement.all
+    @mmc_items = MmcItem.all
   end
 
   def create
@@ -12,24 +12,39 @@ class PromoCodesController < ApplicationController
   end
 
   def update_mmc_categories
-    mmc_vendor = MmcVendor.find(params[:mmc_vendor_id])
+    mmc_vendor = MmcVendor.find(params[:mmc_vendors_id])
     @mmc_categories = mmc_vendor.mmc_categories.map{ |c| [c.name, c.id]}.insert(0, "Select MMC Category")
     @mmc_placements = mmc_vendor.mmc_placements.map{ |p| [p.name, p.id]}.insert(0, "Select MMC Placement")
-    @mmc_items = mmc_vendor.mmc_items.map { |i| [i.name, i.id]}.insert(), "Select MMC Item")
+    @mmc_items = mmc_vendor.mmc_items.map { |i| [i.name, i.id]}.insert(0, "Select MMC Item")
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def update_mmc_placements
-    mmc_category = MmcCategory.find(params[:mmc_category_id])
+    mmc_category = MmcCategory.find(params[:mmc_categories_id])
     @mmc_placements = mmc_category.mmc_placements.map{ |p| [p.name, p.id]}.insert(0, "Select placement")
     @mmc_items = mmc_category.mmc_items.map{ |i| [i.name, i.id]}.insert(0, "Select item")
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def update_mmc_items
-    mmc_placement = MmcPlacement.find(params[:mmc_placement_id])
+    mmc_placement = MmcPlacement.find(params[:mmc_placements_id])
     @mmc_items = mmc_placement.mmc_items.map { |i| [i.name, i.id]}.insert(0, "Select item")
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def promo_code_params
-    params.require(:promo_code).permite(:friendly_name, :base_url)
+    params.require(:promo_code).permit(:friendly_name, :base_url)
   end
 end
